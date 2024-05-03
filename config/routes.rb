184bb95960b3ef92dbedeb33cde47e4308e1af1d
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-05-03 13:51:06 -0300

    Feature: Creating a Sign Up Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   #GET to #about
   get "about", to: "about#index", as: :about
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   root to: "main#index"
 end
